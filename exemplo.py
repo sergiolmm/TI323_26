0dba2a0f16a2b04cbe95ec14cc4f679b9e718f88
ENZZO FERREIRA DE SOUZA <u25698@lapa07.local>
2026-03-03 16:19:45 -0300

    Conteudo

--- exemplo.py
+++ exemplo.py
@@ -1,6 +1,10 @@
+
 print("Projeto Aula TI323. Assunto - Git Merge e pull request")
 print("Coloque o seu nome na linha abaixo")
 print("Larissa Silva")
 
 print("João Guilherme Murer")
+
+print(˜Enzzo˜)
+
 SERGIO L M MARQUES
