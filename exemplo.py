2559d6144218a4186dc9c1baba759614a3d602f7
Sergio LMM <sergio@unicamp.br>
2026-03-09 15:34:37 -0300

    corrigido fix #1

--- exemplo.py
+++ exemplo.py
@@ -3,9 +3,8 @@ print("Projeto Aula TI323. Assunto - Git Merge e pull request")
 print("Coloque o seu nome na linha abaixo")
 print("Larissa Silva")
 print("João Guilherme Murer")
-SERGIO L M MARQUES
+print("SERGIO L M MARQUES")
 print("Levi Souza")
 print("vitoria")
 print("rafa")
-
-teste do push remoto
+print("teste do push remoto")
